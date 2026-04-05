2a18b9630a3c8ad3c7a53fe392ad50af0fb423f9
FOUNDERLOQE <foundersdesk@loqe.co.in>
2026-04-05 20:52:35 +0530

    Update queries.js

--- src/lib/queries.js
+++ src/lib/queries.js
@@ -29,3 +29,42 @@ export const clientProfilesQuery = `
     createdAt
   }
 `
+export const clientProfilesQuery = `
+  *[_type == "clientProfile"] | order(createdAt desc){
+    _id,
+    fullName,
+    clientType,
+    cityOfResidence,
+    tripType,
+    tripLengthDays,
+    travellerCount,
+    budgetBand,
+    originCity,
+    autoSummary,
+    travelSignalTags,
+    createdAt
+  }
+`
+
+export const clientProfileDetailQuery = `
+  *[_type == "clientProfile" && _id == $id][0]{
+    _id,
+    fullName,
+    email,
+    phone,
+    nationality,
+    cityOfResidence,
+    clientType,
+    travellerComposition,
+    relationshipManagerNotes,
+    tripType,
+    tripLengthDays,
+    travellerCount,
+    budgetBand,
+    originCity,
+    autoSummary,
+    travelSignalTags,
+    profilePayload,
+    createdAt
+  }
+`
